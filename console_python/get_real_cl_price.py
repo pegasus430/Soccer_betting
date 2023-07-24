7b33296b51ccd57d4d1317df9bac48fec9d9de0d
ilancerdev <hitech.proton@gmail.com>
2023-07-24 04:39:06 +0100

    updte new week 709

--- console_python/get_real_cl_price.py
+++ console_python/get_real_cl_price.py
@@ -622,8 +622,8 @@ def main():
     # for C_weeknumber in range(691, 699):
     # 	get_realprice_toRealPriceTable_perweek(C_weeknumber)
    
-    get_realprice_toRealPriceTable_perweek(708)
-    matching_realpriceid_toSeasonMatchPlanColumn(708)
+    get_realprice_toRealPriceTable_perweek(709)
+    matching_realpriceid_toSeasonMatchPlanColumn(709)
     
 	
 if __name__ == "__main__":
